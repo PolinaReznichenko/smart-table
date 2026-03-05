6b763cfd6b474dca339675c7d9adf650a28bee9b
Polina Reznichenko <polrez@yandex.ru>
2026-03-05 22:53:09 +0300

    feat: added filtering via the server

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,25 +1,23 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    // заполнение выпадающих списков опциями из справочников с сервера (продавцов, покупателей, ...)
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes)                               // Получаем ключи из объекта (массив имён полей)
+          .forEach((elementName) => {                      // Перебираем по именам
+            elements[elementName].append(                  // в каждый элемент добавляем опции
+                ...Object.values(indexes[elementName])     // формируем массив имён, значений опций
+                    .map(name => {                         // устанавливаем name как значение и текстовое содержимое
+                        const el = document.createElement('option');
+                        el.textContent = name;
+                        el.value = name;
+                        return el;
+                        })
+            )
+        })
+    }
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                  // Получаем ключи из объекта
-      .forEach((elementName) => {                         // Перебираем по именам
-        elements[elementName].append(                     // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                      // используйте name как значение и текстовое содержимое
-                        const optionTag = document.createElement("option");
-                        optionTag.value = name;
-                        optionTag.textContent = name;
-                        return optionTag
-                      })
-        )
-     })
-
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    //формирование части запроса к серверу, отвечающей за фильтрацию
+    const applyFiltering = (query, state, action) => {
+        // код с обработкой очистки поля
         if (action && action.name === "clear") {
             const filterInput = action.parentElement.querySelector("input");
             filterInput.value = "";
@@ -27,7 +25,22 @@ export function initFiltering(elements, indexes) {
             state[fieldName] = "";
         }
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+        // @todo: #4.5 — отфильтровать данные, используя компаратор
+        // сбор значений фильтров
+        const filter = {};
+        Object.keys(elements).forEach(key => {  // проходим по всем ключам объекта (элементам фильтров)
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра (например, { 'filter[seller]': 'Иванов' })
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу (не меняя исходный query)
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
