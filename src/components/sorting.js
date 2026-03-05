e0d60e7ef6e9e9d69f888ff93a46f4deb92dbd1a
Polina Reznichenko <polrez@yandex.ru>
2026-03-06 00:54:09 +0300

    feat: added sorting via the server

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,15 +1,15 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import {sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
+    return (query , state, action) => {
         let field = null; // поле, по которому сортируем
-        let order = null; // направление сортировки 
+        let order = null; // направление сортировки (up, down, none)
 
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
-            action.dataset.value = sortMap[action.dataset.value];
-            field = action.dataset.field;
-            order = action.dataset.value;
+            action.dataset.value = sortMap[action.dataset.value];  // переключаем состояние
+            field = action.dataset.field;  // запоминаем поле
+            order = action.dataset.value;  // запоминаем новое направление сортировки
 
             // @todo: #3.2 — сбросить сортировки остальных колонок
             columns.forEach(column => {
@@ -19,14 +19,17 @@ export function initSorting(columns) {
             });
         } else {
             // @todo: #3.3 — получить выбранный режим сортировки
+            // если не сортировка или первый рендер
             columns.forEach(column => {
-                if (column.dataset.value !== 'none') {
+                if (column.dataset.value !== 'none') {  // берем поле и направление у колонки не none, чтобы сортировка применялась согласно ранее выбранной колонке
                     field = column.dataset.field;
                     order = column.dataset.value;
                 }
             });
         }
 
-        return sortCollection(data, field, order);
+         const sort = (field && order !== 'none') ? `${field}:${order}` : null; // сохраним в переменную параметр сортировки в виде field:direction
+
+        return sort ? Object.assign({}, query, { sort }) : query; //  если есть сортировка, добавляем в запрос, если нет, то не трогаем query
     }
 }
